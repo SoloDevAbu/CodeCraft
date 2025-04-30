8928ddf3f030b0a23e2d44d1a71fcaa7c52a60d8
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2025-05-01 00:33:24 +0530

    init

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import { prisma } from "@repo/db";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      
+    </div>
+  );
+}
