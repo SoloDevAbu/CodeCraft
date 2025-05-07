6310df0102a13c3cfbf4ddca66162be18a43a64f
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2025-05-07 17:07:37 +0530

    Simple appbar

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,5 +1,4 @@
 import Appbar from "@/components/appbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- apps/client/components/appbar.tsx
+++ apps/client/components/appbar.tsx
@@ -1,21 +1,36 @@
-"use client"
+"use client";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 export default function Appbar() {
-    const { data: session } = useSession();
-    return (
+  const { data: session } = useSession();
+  return (
+    <div className="flex justify-between items-center p-4">
         <div>
+            <Image
+                src="/image.png"
+                alt="Logo"
+                width={60}
+                height={60}
+                className="rounded-full"
+            />    
+        </div>
+
+        <div className="flex items-center justify-between gap-4 bg-gray-200 p-3 rounded-full px-5 shadow-sm shadow-blue-950">
+            <p className="text- font-semibold hover:text-blue-800 cursor-pointer">About Us</p>
+            <p className="text- font-semibold hover:text-blue-800 cursor-pointer">Contact Us</p>
+
             {session ? (
                 <div>
-                    <p>Signed in as {session.user?.name}</p>
-                    <button onClick={() => signOut()}>Sign out</button>
+                <Button onClick={() => signOut()}>Sign out</Button>
                 </div>
             ) : (
                 <div>
-                    <p>Not signed in</p>
-                    <button onClick={() => signIn()}>Sign in</button>
+                <Button onClick={() => signIn()}>Sign in</Button>
                 </div>
             )}
         </div>
-    )
+    </div>
+  );
 }
